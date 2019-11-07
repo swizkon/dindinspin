43365ee9f53952dd621fb37662958e0d69f0214f
Jonas Jerndin <Jonas.Jerndin.ext@toyota-industries.eu>
2019-11-07 15:31:27 +0100

    Initial test of thre GraphQL Conventions library

---
+++ src/DinnersGQL/Graph/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GraphQL.Conventions;
+using System.Reflection;
+
+namespace DinnersGQL.Graph
+{
+    internal sealed class Mutation
+    {
+    }
+
+    internal sealed class Query
+    {
+    }
+
+    internal sealed class UserContext : IUserContext
+    {
+    }
+
+    internal sealed class Injector : IDependencyInjector
+    {
+        private readonly IServiceProvider _provider;
+
+        public Injector(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public object Resolve(TypeInfo typeInfo) => _provider.GetService(typeInfo);
+    }
+}
---
+++ src/DinnersGQL/Startup.cs
@@ -0,0 +1,64 @@
+
+//using DinnersGQL.Graph;
+//using GraphQL.Conventions;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+
+//using Microsoft.Extensions.Hosting;
+//using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+
+using GraphQL.Conventions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Schema = DinnersGQL.Graph;
+
+namespace DinnersGQL
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(provider => new GraphQLEngine()
+                .WithFieldResolutionStrategy(FieldResolutionStrategy.Normal)
+                .BuildSchema(typeof(SchemaDefinition<Schema.Query, Schema.Mutation>)));
+
+            services.AddScoped<IDependencyInjector, Schema.Injector>();
+            services.AddScoped<IUserContext, Schema.UserContext>();
+            services.AddScoped<Schema.Query>();
+            services.AddScoped<Schema.Mutation>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
