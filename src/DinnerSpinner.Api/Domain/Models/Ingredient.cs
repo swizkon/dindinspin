f0f5329b223c19cb1bdc605ab646942e8156b087
Jonas Jerndin <jonas@queenslab.se>
2020-08-08 02:19:30 +0200

    Add swagger docs

--- src/DinnerSpinner.Api/Domain/Models/Ingredient.cs
+++ src/DinnerSpinner.Api/Domain/Models/Ingredient.cs
@@ -1,17 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace DinnerSpinner.Api.Domain.Models
 {
     public class Ingredient
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string Name { get; set; }
 
         public bool PlantBased { get; set; }
--- src/DinnerSpinner.Api/Startup.cs
+++ src/DinnerSpinner.Api/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 namespace DinnerSpinner.Api
@@ -31,24 +30,46 @@ namespace DinnerSpinner.Api
 
             services.AddSingleton<SpinnerService>();
 
+            services.AddSwaggerDocumentation();
+
             services.AddControllers();
+
+            services.AddCors(options => {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.ToUpper() == "DEVELOPMENT")
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseRouting();
 
+            app.UseSwaggerDocumentation();
+
+            app.UseCors("AllowAll");
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(config => {
+                config.MapControllers();
+            });
         }
     }
 }
